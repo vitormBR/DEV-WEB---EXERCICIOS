d53d3966854af686a77cc00d53d8684bc9b24b47
VitorM <nunesvitorm09@gmail.com>
2026-05-28 12:39:07 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+
+app.engine('handlebars', exphbs.engine({defaultLayout:false}));
+app.set('view engine', 'handlebars');
 
 
 app.get('/', (req, res) =>{

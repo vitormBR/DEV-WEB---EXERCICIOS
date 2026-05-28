9fd6dbc477b82716839f08a77cea913c377ee960
VitorM <nunesvitorm09@gmail.com>
2026-05-28 11:28:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Testando o express!');
+});
+
+
+app.get('/', (req, res) =>{
+  res.send("Bem-vindo ao sistema")
+})
+
+app.listen(
+    3000, 
+    () => console.log(`Servidor em execução`)
+);

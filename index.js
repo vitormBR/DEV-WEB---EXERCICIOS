f83a7026a765f168b775b38c462793d81c051f61
VitorM <nunesvitorm09@gmail.com>
2026-05-28 11:40:52 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 
 
-
 app.get('/', (req, res) =>{
   res.send("Bem-vindo ao sistema")
+});
+
+app.get('/aplicação', (req, res) =>{
+  res.send("Este repositório serve para hospedar os exercícios e é uma mensagem da aplicação")
 })
 
 app.listen(

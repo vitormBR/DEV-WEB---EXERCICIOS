6711092afaa69a8f4a80bf2bb3477ab39618a350
VitorM <nunesvitorm09@gmail.com>
2026-05-28 11:47:08 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ app.get('/aplicação', (req, res) =>{
   res.send("Este repositório serve para hospedar os exercícios e é uma mensagem da aplicação")
 });
 
+app.get('/contato', (req, res) =>{
+  res.json({
+  "email": "contato@email.com",
+  "telefone": "(81) 99999-9999"
+})
+});
+
 app.listen(
     3000, 
     () => console.log(`Servidor em execução`)

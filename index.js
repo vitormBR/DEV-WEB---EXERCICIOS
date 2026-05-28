c43a73b6199b38dc2bfa523698b185ffaf79a520
VitorM <nunesvitorm09@gmail.com>
2026-05-28 11:54:19 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -21,6 +21,10 @@ app.get('/erro', (req, res) =>{
   res.status(404).send("Página não encontrada")
 });
 
+app.get('/inicio', (req, res ) =>{
+  res.redirect('/')
+});
+
 app.listen(
     3000, 
     () => console.log(`Servidor em execução`)
